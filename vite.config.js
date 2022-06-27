3a1cf5a804461d222684f843c7b53fa5d15ef6d3
Yasiel Hernández Portal <yasielhp@gmail.com>
2022-06-27 12:13:52 +0100

    deploy gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://yasielken.github.io/Simple-Giphy-App/",
   plugins: [react()]
 })
