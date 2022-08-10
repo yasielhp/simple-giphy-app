5e8926c3f6438d1a2ba5593bdd7c01dec12fad0f
Yasiel Hernández Portal <yasielhp@Workstation.local>
2022-08-10 15:30:31 +0100

    deploy in netlify

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://yasielken.github.io/Simple-Giphy-App/",
   plugins: [react()]
 })
